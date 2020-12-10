b67b31ce7f7753aea8573dfcdccb0b8f0f04ac8e
marco <marco.sciaini@indblik.io>
2020-12-10 20:07:14 +0100

    fix while loop

--- R/track_mouse.R
+++ R/track_mouse.R
@@ -2,7 +2,6 @@ track_mouse <- function(time,
                         file) {
 
   #### needs consistency check here !!!!
-
   time_h <- as.numeric(strsplit(time, '([a-z])')[[1]][1])
   time_m <- as.numeric(strsplit(time, '([a-z])')[[1]][2])
   time_s <- as.numeric(strsplit(time, '([a-z])')[[1]][3])
@@ -13,19 +12,16 @@ track_mouse <- function(time,
 
   system_info <- Sys.info()
 
-  ## run under macos ----
-  if (system_info["sysname"] == "Darwin") {
+  ## run under unix ----
+  if (system_info["sysname"] != "Windows") {
     if (time == "inf") {
       system(paste0(
         'secs=', internal_time,
-        'endTime=$(( $(date +%s) + secs ))
-
-        while [ $(date +%s) -lt $endTime ]; do
-        xdotool getmouselocation | sed -E "s/ screen:0 window:[^ ]*|x:|y://g"  >>', file,
-        'done'
+        '; endTime=$(( $(date +%s) + secs )); while [ $(date +%s) -lt $endTime ]; do xdotool getmouselocation | sed -E "s/ screen:0 window:[^ ]*|x:|y://g"  >> ',
+        file,
+        '; done'
       ))
     } else {
-
       system(paste0(
         'while true; do xdotool getmouselocation | sed -E "s/ screen:0 window:[^ ]*|x:|y://g"  >> ',
         file,
@@ -38,5 +34,4 @@ track_mouse <- function(time,
 }
 
 
-strsplit(time, '([a-z])')
 
