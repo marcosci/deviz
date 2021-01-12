055c2b2859bbbdd1d5c4d2cfa7a5dae6323ff9d9
marco <marco.sciaini@indblik.io>
2021-01-12 12:19:48 +0100

    :sparkles: improve calc_dist

--- R/calc_dist.R
+++ R/calc_dist.R
@@ -1,35 +1,38 @@
 #' calc_dist
 #'
-#' @param file Mouse track containing x any y coordinates#'
+#' @param track Mouse track containing x any y coordinates
+#' @param cumulative Return distance as cumulative sum or not, logical, default is true
 #'
 #' @details
-#' Calculate distance in pixels between track points
+#' Calculate distance in pixels between track points that are of type 'Move'.
 #'
-#' @return mouse track including distance#'
+#' @return mouse track including distance
 #' @export
 #'
 #' @examples
-#' calc_dist(mouse_track)
+#' calc_dist(mouse_df)
 #'
+calc_dist <- function(track, cumulative = TRUE) {
 
-calc_dist <- function(file) {
+  #### filter for mouse movement ----
+  track <- track %>%
+    dplyr::filter(type == "Move")
 
   #### initialize distance column ----
-
-  file["dist"] <- as.numeric(NA)
+  track["dist"] <- as.numeric(NA)
 
   #### get number of rows ----
-
-  n <- nrow(file)
+  n <- nrow(track)
 
   #### calculate euclidean distance between points ----
-
-  file$dist[2:n] <- sqrt((file$x[2:n] - file$x[1:n-1]) ^ 2 + (file$y[2:n] - file$y[1:n-1]) ^ 2)
+  track$dist[2:n] <- sqrt((track$x[2:n] - track$x[1:n-1]) ^ 2 + (track$y[2:n] - track$y[1:n-1]) ^ 2)
 
   #### get rid of NAs ----
+  track$dist[is.na(track$dist)] <- 0
 
-  file$dist[is.na(file$dist)] <- 0
+  #### to be cumulative or not, that is the question ----
+  if(cumulative) track$dist <- cumsum(track$dist)
 
-  return(file)
+  return(track)
 
 }
